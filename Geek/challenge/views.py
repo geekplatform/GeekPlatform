2a6b8e3a942c41014ea1f8a54776353d6cff5f4e
LJLS-l <2208032365@qq.com>
2019-07-16 22:26:59 +0800

    challenge

--- Geek/challenge/views.py
+++ Geek/challenge/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Challenge
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def index(request):
+    challenge_list=Challenge.objects.all().order_by('-created_time')
+    return render(request,'test.html',context={'challenge_list':challenge_list})
